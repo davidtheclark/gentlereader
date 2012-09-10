5e805ec3ad671a95a449bc4e555ca68ee82cc191
David <david.dave.clark@gmail.com>
2012-09-10 14:41:55 -0500

    Changed style of selection-list-items.

--- static/anthologist/js/models.js
+++ static/anthologist/js/models.js
@@ -98,7 +98,7 @@ var RecentContentSet = Backbone.Collection.extend({
 	url: '/api/recent/',
 	initialize: function(models, options) {
 		if (options && options.hasOwnProperty('restriction') && typeof options['restriction'] !== 'undefined') {
-			this.url += '/' + options['restriction'];
+			this.url += options['restriction'];
 		}
 	}
 });
