1dc3346727540f830a794bd2ea33249601d84208
David <david.dave.clark@gmail.com>
2013-02-15 08:51:32 -0700

    Changed reCaptcha key setup.

--- forms.py
+++ forms.py
@@ -6,7 +6,4 @@ class ContactForm(forms.Form):
     cc_myself = forms.BooleanField(required=False)
     subject = forms.CharField()
     message = forms.CharField(widget=forms.widgets.Textarea())
-    captcha = ReCaptchaField(
-        public_key='6LdeEN0SAAAAAG2rSR1WrF4SkXsYHqG5N-sFn3z6',
-        private_key='6LdeEN0SAAAAAMMZCm1C_KcW5JqkRUjaWKUx2bD5',
-    )
+    captcha = ReCaptchaField()
