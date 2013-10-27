327aa20c47c520839f9b9d06d6be5440a52e0bad
David Clark <david.dave.clark@gmail.com>
2013-10-27 11:16:19 -0700

    Fixing period typo

--- static/gentlereader/scripts/templates/selListItemTempl.js
+++ static/gentlereader/scripts/templates/selListItemTempl.js
@@ -8,10 +8,10 @@ var interp;
 var __indent = [];
 buf.push('\n<section><a');
 buf.push(attrs({ 'href':('/selections/' + (model.get("slug")) + '') }, {"href":true}));
-buf.push('>\n    <h3 class="selectioninlist-title">' + escape((interp = model.get("title")) == null ? '' : interp) + '</h3>\n    <div class="selectioninlist-details"><span class="selectioninlist-author">' + escape((interp = model.get("author")) == null ? '' : interp) + '.&#32;</span>');
+buf.push('>\n    <h3 class="selectioninlist-title">' + escape((interp = model.get("title")) == null ? '' : interp) + '</h3>\n    <div class="selectioninlist-details"><span class="selectioninlist-author">' + escape((interp = model.get("author")) == null ? '' : interp) + '</span>');
 if ( model.get("from_display"))
 {
-buf.push('' + ((interp = model.get("from_display")) == null ? '' : interp) + '');
+buf.push('.&#32;' + ((interp = model.get("from_display")) == null ? '' : interp) + '');
 }
 buf.push('&#32;(' + ((interp = model.get("date_display")) == null ? '' : interp) + '). <span class="selectioninlist-posttime">Posted ' + escape((interp = model.get("date_entered")) == null ? '' : interp) + '</span>\n    </div></a></section>\n<section class="selectioninlist-teaser"><span class="teaser-text hyphenate">');
 var __val__ = model.get("teaser")
